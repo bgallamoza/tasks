7a8006b460ee5b9fd4da85b867c343b6d7ac4f40
bgallamoza <brennan.gallamoza@gmail.com>
2022-02-10 14:45:24 -0500

    implemented and successfully tested addOption and mergeQuestion

--- src/objects.ts
+++ src/objects.ts
@@ -131,7 +131,7 @@ export function duplicateQuestion(id: number, oldQuestion: Question): Question {
  * Check out the subsection about "Nested Fields" for more information.
  */
 export function addOption(question: Question, newOption: string): Question {
-    return question;
+    return { ...question, options: [...question.options, newOption] };
 }
 
 /**
@@ -148,5 +148,11 @@ export function mergeQuestion(
     contentQuestion: Question,
     { points }: { points: number }
 ): Question {
-    return contentQuestion;
+    return {
+        ...contentQuestion,
+        points: { points }.points,
+        published: false,
+        id: id,
+        name: name
+    };
 }
