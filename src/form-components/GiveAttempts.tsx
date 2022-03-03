f876bb68816904660269a8a6b1d67796bed386bb
bgallamoza <brennan.gallamoza@gmail.com>
2022-03-02 21:43:20 -0500

    implemented GiveAttempts.tsx

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,51 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+
+type ChangeEvent = React.ChangeEvent<
+    HTMLTextAreaElement | HTMLInputElement | HTMLSelectElement
+>;
 
 export function GiveAttempts(): JSX.Element {
+    const [request, setRequest] = useState<number>(NaN);
+    const [attempts, setAttempts] = useState<number>(3);
+
+    function checkNewRequest(event: ChangeEvent) {
+        const newRequest = parseInt(event.target.value);
+        if (!isNaN(newRequest) && newRequest >= 0) {
+            setRequest(newRequest);
+            setAttempts(newRequest);
+        }
+    }
+
+    function useButton(): JSX.Element {
+        return (
+            <Button
+                disabled={attempts <= 0}
+                onClick={() => setAttempts(attempts - 1)}
+            >
+                use
+            </Button>
+        );
+    }
+
+    function gainButton(): JSX.Element {
+        return <Button onClick={() => setAttempts(attempts + 1)}>gain</Button>;
+    }
+
     return (
         <div>
             <h3>Give Attempts</h3>
+            <Form.Group controlId="formAttempts">
+                <Form.Label>Request Attempts:</Form.Label>
+                <Form.Control type="number" onChange={checkNewRequest} />
+            </Form.Group>
+            <p>Requested Attempts: {isNaN(request) ? "" : request}</p>
+            <p>Current Attempts: {attempts}</p>
+            <div>
+                {useButton()}
+                {gainButton()}
+            </div>
         </div>
     );
 }
